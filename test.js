d6ede1b168764a806f35b12797a4a341c265d167
EGOIST <0x142857@gmail.com>
2016-04-29 16:31:44 +0800

    improve coverage

--- test.js
+++ test.js
@@ -7,10 +7,48 @@ test('camelcased keys', t => {
   t.true(cli.argv.flags.hideProgress)
 })
 
+test('output version', t => {
+  const cli = cac(`wow`)
+  cli.parse(['--version'])
+  process.stdout.on('data', data => {
+    t.true(data.indexOf('.') !== -1)
+  })
+})
+
 test('output help', t => {
   const cli = cac(`wow`)
   cli.parse(['--help'])
   process.stdout.on('data', data => {
     t.true(data.indexOf('wow') !== -1)
   })
+})
+
+test('help is array', t => {
+  const cli = cac(['wow'])
+  cli.parse(['--help'])
+  process.stdout.on('data', data => {
+    t.true(data.indexOf('wow') !== -1)
+  })
+})
+
+test('command', t => {
+  const cli = cac()
+  cli.command('r, rm, remove', function () {
+    console.log('remove')
+  })
+  cli.parse(['remove'])
+  process.stdout.on('data', data => {
+    t.true(data.indexOf('remove') !== -1)
+  })
+})
+
+test('hanle error', t => {
+  const cli = cac()
+  cli.command('foo', function* () {
+    throw new Error('lah')
+  })
+  cli.onError = function () {
+    t.pass()
+  }
+  cli.parse(['foo'])
 })
