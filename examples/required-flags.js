51802476b1f83222b27f269c80ca4b4758295663
EGOIST <0x142857@gmail.com>
2017-10-15 20:33:21 +0800

    refactor example for required flags

--- examples/required-flags.js
+++ examples/required-flags.js
@@ -4,13 +4,22 @@ const cac = require('../src').default
 const cli = cac()
 
 cli.command('a', {
-  desc: 'command a',
-  requiredFlags: ['foo']
+  desc: 'command a'
+}).option('foo', {
+  required: true, // <-- Make it a required flag!
+  desc: 'foo is a flag for command a'
 })
 
 cli.command('b', {
-  desc: 'command b',
-  requiredFlags: ['bar']
+  desc: 'command b'
+}).option('bar', {
+  required: true, // <-- Make it a required flag!
+  desc: 'bar is a flag for command b'
+}).option('baz', {
+  required: true, // <-- Make it a required flag!
+  desc: 'baz is a flag for command b'
+}).option('zoo', {
+  desc: 'bar is a flag for command b'
 })
 
 cli.use(requiredFlags())
@@ -20,13 +29,14 @@ function requiredFlags() {
     cli.on('parsed', (command, input, flags) => {
       if (!command) return
 
-      const requiredFlags = command.command.requiredFlags || []
-      const matched = []
-      requiredFlags.forEach(flag => {
-        if (!(flag in flags)) matched.push(flag)
-      })
-      if (matched.length > 0) {
-        console.error(`missing flags: ${matched.join(', ')}`)
+      const missingRequiredOptions = command.options.options
+        .filter(option => {
+          const isRequired = option.required
+          const isMissing = typeof flags[option.name] === 'undefined'
+          return isMissing && isRequired
+        })
+      if (missingRequiredOptions.length > 0) {
+        console.log(`Missing flags: ${missingRequiredOptions.map(option => option.name).join(', ')}`)
         process.exit(1)
       }
     })
@@ -34,3 +44,6 @@ function requiredFlags() {
 }
 
 cli.parse()
+
+// Preview
+// https://ooo.0o0.ooo/2017/10/15/59e354e9e06bc.png
