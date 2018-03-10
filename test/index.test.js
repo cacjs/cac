ce3312fd5784c8af85964ae995ab199307f35572
EGOIST <0x142857@gmail.com>
2018-03-11 01:21:41 +0800

    Revert "do not use snapshots"
    This reverts commit 432d6d69de68549432e8c4c52244b99a8314e249.

--- test/index.test.js
+++ test/index.test.js
@@ -3,29 +3,26 @@ import test from 'ava'
 import execa from 'execa'
 
 test('help message when no command', async t => {
-  const file = 'help-message-when-no-command'
   const { stdout } = await execa('node', [
-    path.join(__dirname, 'fixtures', file),
+    path.join(__dirname, 'fixtures/help-message-when-no-command.js'),
     '--help'
   ])
-  t.true(stdout.includes(`${file} <command> [options]`))
+  t.snapshot(stdout)
 })
 
 test('help message for sub command', async t => {
-  const file = 'help-message-for-sub-command'
   const { stdout } = await execa('node', [
-    path.join(__dirname, 'fixtures', file),
+    path.join(__dirname, 'fixtures/help-message-for-sub-command.js'),
     'sub',
     '--help'
   ])
-  t.true(stdout.includes(`${file} sub [options]`))
+  t.snapshot(stdout)
 })
 
 test('help message for wildcard command', async t => {
-  const file = 'help-message-for-wildcard-command'
   const { stdout } = await execa('node', [
-    path.join(__dirname, 'fixtures', file),
+    path.join(__dirname, 'fixtures/help-message-for-sub-command.js'),
     '--help'
   ])
-  t.true(stdout.includes(`${file} <command> [options]`))
+  t.snapshot(stdout)
 })
