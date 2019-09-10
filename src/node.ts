1042c34be2c642eff80d11cc885fd97c4ee1164d
迷渡 <justjavac@gmail.com>
2019-09-10 21:05:06 +0800

    fix: Replace Deno.platform with Deno.build (#53)

--- src/node.ts
+++ src/node.ts
@@ -10,5 +10,5 @@ export const exit = (code: number) => {
 export const processArgs = deno ? ['deno'].concat(Deno.args) : process.argv
 
 export const platformInfo = deno
-  ? `${Deno.platform.os}-${Deno.platform.arch} deno-${Deno.version.deno}`
+  ? `${Deno.build.os}-${Deno.build.arch} deno-${Deno.version.deno}`
   : `${process.platform}-${process.arch} node-${process.version}`
