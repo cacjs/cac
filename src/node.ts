d92f5a818cc7a5450d5a84041b45db03dbd0a52e
Guilherme Rodrigues <guilherme@vtex.com>
2020-03-08 18:18:29 -0300

    Fix deno argv handling

--- src/node.ts
+++ src/node.ts
@@ -2,8 +2,14 @@ declare let window: any
 declare let Deno: any
 
 const deno = typeof window !== 'undefined' && window.Deno
+const denoScriptPath =
+  deno && typeof window !== 'undefined' && window.location.pathname
 
-export const processArgs = deno ? ['deno'].concat(Deno.args) : process.argv
+// Adds deno executable and script path to processArgs as "compatibility" layer for node
+// See https://github.com/cacjs/cac/issues/69
+export const processArgs = deno
+  ? ['deno', denoScriptPath].concat(Deno.args)
+  : process.argv
 
 export const platformInfo = deno
   ? `${Deno.build.os}-${Deno.build.arch} deno-${Deno.version.deno}`
