b99cd9a83022a51edab745c05b402a9fb0f7e890
pengfeipang <1341993247@qq.com>
2022-07-05 22:47:21 +0800

    feat: MAX

--- src/deno.ts
+++ src/deno.ts
@@ -1,6 +1,7 @@
 // Ignore the TypeScript errors
 // Since this file will only be used in Deno runtime
 
-export const processArgs = ['deno', 'cli'].concat(Deno.args)
+const _ARGS = ['deno', 'cli']
+export const processArgs = [..._ARGS, ...Deno.args]
 
 export const platformInfo = `${Deno.build.os}-${Deno.build.arch} deno-${Deno.version.deno}`
