fc610ac6b62a1df50a7858ec2bdbda721aacfc48
pengfeipang <1341993247@qq.com>
2022-07-05 22:53:51 +0800

    feat: use es6 for update

--- src/deno.ts
+++ src/deno.ts
@@ -1,7 +1,6 @@
 // Ignore the TypeScript errors
 // Since this file will only be used in Deno runtime
 
-const _ARGS = ['deno', 'cli']
-export const processArgs = [..._ARGS, ...Deno.args]
+export const processArgs = [...['deno', 'cli'], ...Deno.args]
 
 export const platformInfo = `${Deno.build.os}-${Deno.build.arch} deno-${Deno.version.deno}`
